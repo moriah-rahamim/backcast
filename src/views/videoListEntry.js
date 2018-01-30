417dcc7544de7df0f6facb28b321a47639a303a4
moriah-rahamim <moriahfaye@gmail.com>
2018-01-30 11:48:24 -0800

    Add a descriptive comment to render function

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,7 +1,7 @@
 var VideoListEntryView = Backbone.View.extend({
 
-
   render: function() {
+    // Adding template html to this element
     this.$el.html(this.template(this.model.attributes));
     return this;
   },
