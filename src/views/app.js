e764469cfd2d5559d113f88f21b634dce3cc8e20
moriah-rahamim <moriahfaye@gmail.com>
2018-01-30 11:46:20 -0800

    Render videoListView after initializing it

--- src/views/app.js
+++ src/views/app.js
@@ -4,13 +4,11 @@ var AppView = Backbone.View.extend({
 
   initialize: function() {
     this.videos = new Videos(window.exampleVideoData); // Collection - of Video models
-    
-    
     this.render(); // render the AppView
     // We had to generate videoList AFTER appview was
     // rendered - videoList looked for .list
-    this.videoList = new VideoListView(this.videos);
-    // console.log(this.videos.models[0]);
+    this.videoList = new VideoListView({collection: this.videos});
+    this.videoList.render();
   },
 
 
