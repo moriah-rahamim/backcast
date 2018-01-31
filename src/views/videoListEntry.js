9d171e415e409bc645a798c239622976a4dde311
moriah-rahamim <moriahfaye@gmail.com>
2018-01-30 17:26:58 -0800

    Add event listeners to video list entry views
    Need to add specific behaviors to select event later

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,8 +1,19 @@
 var VideoListEntryView = Backbone.View.extend({
 
+  // Helper function that set event listeners; will call after items on DOM are rendered.
+  setEventListeners: function() {
+    var currentView = this;
+    this.$el.find('.video-list-entry-title').on('click', function() {
+      currentView.model.select.call(currentView);
+    });
+    this.on('select', function() {console.log('hallo');});
+
+  },
+
   render: function() {
     // Adding template html to this element
     this.$el.html(this.template(this.model.attributes));
+    this.setEventListeners();
     return this;
   },
 
